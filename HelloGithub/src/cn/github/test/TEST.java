20188d2c9ccef8c085f8bce1591ba58bef46e78e
zhaolijin <719459589@qq.com>
2016-01-03 19:43:39 +0800

    测试一个有序的Set

--- HelloGithub/src/cn/github/test/TEST.java
+++ HelloGithub/src/cn/github/test/TEST.java
@@ -1,5 +1,10 @@
 package cn.github.test;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class TEST {
 
 	/**
@@ -7,7 +12,19 @@ public class TEST {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello GitHub.");
-		System.out.println("SUCCESS.");
+//		Set<String> set = new HashSet<String>();//无序
+		Set<String> set = new LinkedHashSet<String>();//有序(输入顺序)
+		set.add("a");
+		set.add("b");
+		set.add("c");
+		set.add("11");
+		set.add("22");
+		set.add("#");
+		set.add("##");
+		Iterator<String> it = set.iterator();
+		while(it.hasNext()){
+			System.out.println(it.hashCode()+":"+it.next());
+		}
 	}
 
 }
